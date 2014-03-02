9a8cec90bcb2520bddbf27968f3ab36b1f17bb26
Lauren Chilcote <chilcote.lauren@gmail.com>
2014-03-01 20:03:07 -0500

    Excluding files from deploy

--- deploy.sh
+++ deploy.sh
@@ -3,5 +3,7 @@ s3cmd sync -M -P --delete \
     --exclude=.git* \
     --exclude=README.md \
     --exclude=deploy.sh \
+    --exclude=node_modules/* \
+    --exclude=package.json \
     . \
     s3://laurenchilcote.com/
